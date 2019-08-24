5f81000ec9d6f2ef81b2c8b5127a37162017b5f9
SLO-MO-User <tuncel.halit@hotmail.com>
2019-08-25 01:23:48 +0300

    React fronted done

---
+++ public/precache-manifest.b0cd593f599514f8ebdfa30ef851446a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fe21e05da13b579ed2ccc5d2036c1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07fe792b955d4dd59cf5",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "83d1811d749f699633e5",
+    "url": "/static/js/2.620a10ac.chunk.js"
+  },
+  {
+    "revision": "07fe792b955d4dd59cf5",
+    "url": "/static/js/main.def75f17.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
